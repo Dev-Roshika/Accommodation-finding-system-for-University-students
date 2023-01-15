3706a587f39ad6aed16b384c6e6208ec9eb4681a
Your Name <roshikanayanadhara@gmail.com>
2023-01-15 15:37:07 +0530

    initial commit for onboardScreen and set the nevigator

---
+++ Screen/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { StyleSheet, Text,Image,TextInput, View, Button, TouchableOpacity, Pressable } from 'react-native';
+
+function Home() {
+  return (
+    <View  style={{fontWeight:'bold',paddingTop:100,justifyContent:'center',alignContent:'center',alignItems:'center'}}>
+      <Text>Home </Text>
+      <Text>Home </Text>
+      <Text>Home </Text>
+      <Text>Home </Text>
+      <Text>Home </Text>
+      <Text>Home </Text>
+      <Text>Home </Text>
+      <Text>Home </Text>
+    </View>
+  )
+}
+
+export default Home
