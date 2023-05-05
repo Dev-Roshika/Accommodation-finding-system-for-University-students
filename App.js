d2d6a89d7bd26399e08f6c3c86d260638970d8a9
Roshika Nayanadhara <81811433+Dev-Roshika@users.noreply.github.com>
2023-05-06 02:47:49 +0530

    Develop (#8)
    * initial commit for login
    
    * modified Sign up button and login.Add custom fonts and Created a cutom hook to Show/hide password visibility
    
    * modified Sign up button and login.Add custom fonts and Created a cutom hook to Show/hide password visibility (#3) (#4)
    
    merge into feature/login (#2)
    
    * initial commit for onboardScreen and set the nevigator
    
    * Show/Hide eye icon is set correctly (#6)
    
    ---------
    
    Co-authored-by: ishara madushanka <isharamadushankakity@gmail.com>
    Co-authored-by: IsharaMadushanka <60610077+IsharaMadushanka@users.noreply.github.com>

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './Screen/Home';
+import Login from './Screen/Login'
+import OnBoardScreen from './Screen/OnBoardScreen'
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  
+    return (
+      <NavigationContainer >
+        <Stack.Navigator screenOptions={{headerShown:false}}>
+          <Stack.Screen name = "onBordingScreen" component = {OnBoardScreen} />
+          <Stack.Screen name = "login" component = {Login} />
+          <Stack.Screen name = "home" component = {Home} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
