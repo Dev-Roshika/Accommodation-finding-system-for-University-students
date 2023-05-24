42db7871f9f41c2bae0719ef230553ddee55ab0c
Roshika Nayanadhara <81811433+Dev-Roshika@users.noreply.github.com>
2023-05-24 22:08:22 +0530

    Owner side - boarding registration (#15)
    * Validation of password and hashing using bcrypt library
    
    * Creation of navbar component and header components
    
    * Creation of the home page
    
    * Creation of the boardinghouses profile screen
    
    * Changing file name to js to jsx
    
    * Owner - Boarding registration

--- Web App/backend/server.js
+++ Web App/backend/server.js
@@ -1,11 +1,13 @@
-const express = require("express");
-const mysql = require("mysql");
-const cors = require("cors");
-const session = require("express-session");
-const cookieParser = require("cookie-parser");
-const bodyParser = require("body-parser");
-const bcrypt = require("bcrypt");
-//const { response } = require("express");
+import express from "express";
+import mysql from "mysql";
+import cors from "cors";
+import session from "express-session";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import multer from "multer";
+import path from "path";
+import bcrypt from "bcrypt";
+
 const salt = 10; // for bcrypt
 
 const app = express();
@@ -94,6 +96,118 @@ app.post("/student/login", (req, res) => {
     }
   });
 });
+
+///Owner - start
+
+//Cover image upload - start
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images/cover_images");
+  },
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      file.fieldname + "_" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+
+const upload = multer({
+  storage: storage,
+});
+
+app.post("/owner/post-ad", upload.single("coverimage"), (req, res) => {
+  // console.log(req.file) // Can retrieve image information like this
+  // console.log(req.file.filename) // Can retrieve image filename information like this
+  // console.log(req.body.title) // Can retrieve title information like this
+  // console.log(req.body.image) //Cannot retrieve image information like this
+  const img_filename = req.file.filename;
+  const sql =
+    "INSERT INTO `boarding_house` (`Title`, `Description`, `Price`, `Address`, `Boys`, `Girls`, `Facilities`, `Rules`, `ContactNo`, `CoverImage`) VALUES(?)";
+  console.log(img_filename + "___" + req.body.address);
+  const values = [
+    req.body.title,
+    req.body.description,
+    req.body.price,
+    req.body.address,
+    req.body.boys,
+    req.body.girls,
+    req.body.facilities,
+    req.body.rules,
+    req.body.contactno,
+    img_filename,
+  ];
+  db.query(sql, [values], (err, data) => {
+    if (err) return res.json({ Error: "Insert data Error in server" });
+    return res.json({ Status: "Success", RowId: data.insertId });
+  });
+});
+//Cover image upload - end
+
+// multiple image upload - start
+// Configure Multer for file upload
+const storage_multiple = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/images");
+  },
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+const fileFilter = function (req, file, cb) {
+  // Validate file types (you can customize this as per your requirements)
+  const allowedExtensions = [".jpg", ".jpeg", ".png"];
+  const extname = path.extname(file.originalname).toLowerCase();
+  if (allowedExtensions.includes(extname)) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only .jpg, .jpeg, and .png files are allowed"));
+  }
+};
+const upload_multiple = multer({
+  storage: storage_multiple,
+  fileFilter: fileFilter,
+  limits: {
+    fileSize: 5 * 1024 * 1024, // Limit file size to 5MB
+    files: 6, // Limit the number of files to 6
+  },
+}).array("uploadImages", 6); // 'uploadImages' is the field name for the uploaded files
+
+app.post("/images/upload/:id", (req, res) => {
+  const RowId = req.params.id;
+  upload_multiple(req, res, function (err) {
+    if (err) {
+      if (err instanceof multer.MulterError) {
+        // Multer error
+        console.log("Multer Error:", err);
+        res.status(500).json({ error: "File upload error" });
+      } else {
+        // Other errors
+        console.log("Error:", err);
+        res.status(400).json({ error: err.message });
+      }
+    } else {
+      // Files uploaded successfully
+      console.log("Files uploaded:", req.files);
+      const values = req.files ? req.files.map((file) => file.filename) : [];
+      const valuesString = JSON.stringify(values); // Convert array to JSON string
+      const sql = "UPDATE `boarding_house` SET `OtherImages` = ? WHERE `Id` = ?";
+      console.log(RowId);
+      console.log(valuesString);
+      
+      db.query(sql, [valuesString, RowId], (err, data) => {
+        if (err) return res.json({ Error: "Insert data Error in server" });
+        return res.json({ Status: "Success" });
+      });
+    }
+  });
+});
+// multiple image upload - end
+
+///Owner - end
 
 app.get("/logout", (req, res) => {
   req.session.destroy();
---
+++ Web App/frontend/src/Owner/PostAd.jsx
@@ -0,0 +1,111 @@
+import axios from 'axios';
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+
+function PostAd() {
+  const [values, setValues] = useState({
+    title:'',
+    description:'',
+    price:'',
+    address:'',
+    boys:'',
+    girls:'',
+    facilities:'',
+    rules:'',
+    contactno:'',
+  });
+
+  const [file, setFile] = useState();
+  const navigate = useNavigate();
+
+  const handleInput = (event) => {
+    setValues((prev) => ({
+      ...prev,
+      [event.target.name]: [event.target.value],
+    }));
+  };
+
+  const handleFile = (event) => {
+    //console.log(event.target.files[0])
+      setFile(event.target.files[0])
+  };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append('coverimage',file);
+    formData.append('title',values.title);
+    formData.append('description',values.description);
+    formData.append('price',values.price);
+    formData.append('address',values.address);
+    formData.append('boys',values.boys);
+    formData.append('girls',values.girls);
+    formData.append('facilities',values.facilities);
+    formData.append('rules',values.rules);
+    formData.append('contactno',values.contactno);
+      axios
+        .post("http://localhost:8081/owner/post-ad", formData)
+        .then((res) => {
+          if (res.data.Status === "Success") {
+            const RowId = res.data.RowId;
+            console.log(res.data);
+            navigate("/owner/post-ad/success/" + RowId);
+          } else {
+            alert("Error");
+          }
+        })
+        .catch((err) => console.log(err));
+  };
+  return (
+    <div>
+      <div className='d-flex flex-direction-column'>
+        <form action="" onSubmit={handleSubmit}>
+            <div>
+            <label htmlFor="title">Title</label>
+            <input type="text" name='title' onChange={handleInput}/>
+            </div>
+            <div> 
+            <label htmlFor="description">Description</label>
+            <input type="text" name='description' onChange={handleInput}/>
+            </div>
+            <div>
+            <label htmlFor="price">Price</label>
+            <input type="text" name='price' onChange={handleInput}/>
+            </div>
+            <div>
+            <label htmlFor="title">Address</label>
+            <input type="text" name='address' onChange={handleInput}/>
+            </div>
+            <div>
+            <label htmlFor="title">Boys</label>
+            <input type="text" name='boys' onChange={handleInput}/>
+            </div>
+            <div>
+            <label htmlFor="title">Girls</label>
+            <input type="text" name='girls'  onChange={handleInput}/>
+            </div>
+            <div>
+            <label htmlFor="facilities">Facilities</label>
+            <input type="text" name='facilities'  onChange={handleInput}/>
+            </div>
+            <div>
+            <label htmlFor="rules">Rules</label>
+            <input type="text" name='rules'  onChange={handleInput}/>
+            </div>
+            <div>
+            <label htmlFor="contactno">Contact Number</label>
+            <input type="text" name='contactno'  onChange={handleInput}/>
+            </div>
+            <div>
+            <label htmlFor="title">Cover Image</label>
+            <input type="file" name='coverimage'  onChange={handleFile}/>
+            </div>
+            <div>
+            <button type="submit">Post</button>
+            </div>
+        </form>
+        </div>
+    </div>
+  )
+}
+
+export default PostAd
