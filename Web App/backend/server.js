b6eef3cee5bc65e84412effaabf28e7b0efba615
Roshika Nayanadhara <81811433+Dev-Roshika@users.noreply.github.com>
2023-05-30 10:19:49 +0530

    Web dev roshika (#16)
    * Validation of password and hashing using bcrypt library
    
    * Creation of navbar component and header components
    
    * Creation of the home page
    
    * Creation of the boardinghouses profile screen
    
    * Changing file name to js to jsx
    
    * Owner - Boarding registration
    
    * session management and NotFoundError component

--- Web App/backend/server.js
+++ Web App/backend/server.js
@@ -19,6 +19,7 @@ app.use(
   })
 );
 app.use(express.json());
+app.use(express.static('public'));// images folder
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(
@@ -40,17 +41,36 @@ const db = mysql.createConnection({
   database: "software",
 });
 
-app.get("/home", (req, res) => {
-  if (req.session.role) {
-    return res.json({ valid: true, role: req.session.role });
-  } else {
-    return res.json({ valid: false });
-  }
+// app.get("/home", (req, res) => {
+//   if (req.session.role) {
+//     return res.json({ valid: true, role: req.session.role });
+//   } else {
+//     return res.json({ valid: false });
+//   }
+// });
+
+//signup - start
+const signup_storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images/profile_images/student");
+  },
+  filename: (req, file, cb) => {
+    const ext = path.extname(file.originalname); // Get the file extension
+    const filenameWithoutExt = path.basename(file.originalname, ext); // Get the filename without extension
+    cb(
+      null,
+      file.fieldname + "_" + Date.now() +"_"+ filenameWithoutExt + path.extname(file.originalname)
+    );
+  },
 });
 
-app.post("/student/signup", (req, res) => {
+const signup_upload = multer({
+  storage: signup_storage,
+});
+app.post("/student/signup", signup_upload.single("profile_image"), (req, res) => {
+  const img_filename = req.file.filename;
   const sql =
-    "INSERT INTO `user_info` (`FullName`,`UserName`, `UnivRegNo`, `ContactNo`,`Email`,`Faculty`,`Dept`,`PrivateAddress`,`Password`,`Role`) VALUES(?)";
+    "INSERT INTO `student_info` (`FullName`,`UserName`, `UnivRegNo`, `ContactNo`, `ProfileImage`,`Email`,`Faculty`,`Dept`,`Password`) VALUES(?)";
   bcrypt.hash(req.body.password.toString(), salt, (err, hash) => {
     if (err) return res.json({ Error: "Error for hashing password" });
     const values = [
@@ -58,12 +78,11 @@ app.post("/student/signup", (req, res) => {
       req.body.username,
       req.body.univregno,
       req.body.mobile,
+      img_filename,
       req.body.email,
       req.body.faculty,
       req.body.dept,
-      req.body.paddress,
-      hash,
-      req.body.role,
+      hash
     ];
     db.query(sql, [values], (err, result) => {
       if (err) return res.json({ Error: "Insert data Error in server" });
@@ -71,21 +90,26 @@ app.post("/student/signup", (req, res) => {
     });
   });
 });
+//signup - end
 
 app.post("/student/login", (req, res) => {
-  const sql = "SELECT * FROM user_info WHERE `Email` = ?";
+  const sql = "SELECT * FROM student_info WHERE `Email` = ?";
 
   db.query(sql, [req.body.email], (err, result) => {
     if (err) return res.json({ Message: "Login error in server" });
 
     if (result.length > 0) {
+      req.session.email = result[0].Email
+      req.session.role = "student";
+      console.log(req.session.email)
+      console.log(req.session.role)
       bcrypt.compare(
         req.body.password.toString(),
         result[0].Password,
         (err, response) => {
           if (err) return res.json({ Error: "Password compare error" });
           if (response) {
-            return res.json({ Status: "Success" });
+            return res.json({ Status: "Success", Email: req.session.Email, Role: req.session.role });
           } else {
             return res.json({ Error: "Password not matched" });
           }
@@ -98,6 +122,77 @@ app.post("/student/login", (req, res) => {
 });
 
 ///Owner - start
+//signup - start
+const O_signup_storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images/nidphoto");
+  },
+  filename: (req, file, cb) => {
+    const ext = path.extname(file.originalname); // Get the file extension
+    const filenameWithoutExt = path.basename(file.originalname, ext); // Get the filename without extension
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + "-" + filenameWithoutExt + path.extname(file.originalname)
+    );
+  },
+});
+
+const O_signup_upload = multer({
+  storage: O_signup_storage,
+});
+app.post("/owner/signup", O_signup_upload.single("nidphoto"), (req, res) => {
+  console.log(req.file) // Can retrieve image information like this
+  const img_filename = req.file.filename;
+  const sql =
+    "INSERT INTO `owner_info` (`FullName`,`UserName`, `ContactNo`, `Email`, `NidNo`, `NidPhoto`, `PrivateAddress`, `Password`) VALUES(?)";
+  bcrypt.hash(req.body.password.toString(), salt, (err, hash) => {
+    if (err) return res.json({ Error: "Error for hashing password" });
+    const values = [
+      req.body.fullname,
+      req.body.username,
+      req.body.mobile,
+      req.body.email,
+      req.body.nidno,
+      img_filename,
+      req.body.paddress,
+      hash
+    ];
+    db.query(sql, [values], (err, result) => {
+      if (err) return res.json({ Error: "Insert data Error in server" });
+      return res.json({ Status: "Success" });
+    });
+  });
+});
+//signup end
+
+app.post("/owner/login", (req, res) => {
+  const sql = "SELECT * FROM owner_info WHERE `Email` = ?";
+
+  db.query(sql, [req.body.email], (err, result) => {
+    if (err) return res.json({ Message: "Login error in server" });
+
+    if (result.length > 0) {
+      req.session.email = result[0].Email
+      req.session.role = "owner";
+      console.log(req.session.email)
+      console.log(req.session.role)
+      bcrypt.compare(
+        req.body.password.toString(),
+        result[0].Password,
+        (err, response) => {
+          if (err) return res.json({ Error: "Password compare error" });
+          if (response) {
+            return res.json({ Status: "Success", Email: req.session.Email, Role: req.session.role });
+          } else {
+            return res.json({ Error: "Password not matched" });
+          }
+        }
+      );
+    } else {
+      return res.json({ Error: "No email existed" });
+    }
+  });
+});
 
 //Cover image upload - start
 const storage = multer.diskStorage({
@@ -105,9 +200,11 @@ const storage = multer.diskStorage({
     cb(null, "public/images/cover_images");
   },
   filename: (req, file, cb) => {
+    const ext = path.extname(file.originalname); // Get the file extension
+    const filenameWithoutExt = path.basename(file.originalname, ext); // Get the filename without extension
     cb(
       null,
-      file.fieldname + "_" + Date.now() + path.extname(file.originalname)
+      file.fieldname + "_" + Date.now() +"_"+filenameWithoutExt+ path.extname(file.originalname)
     );
   },
 });
@@ -123,13 +220,15 @@ app.post("/owner/post-ad", upload.single("coverimage"), (req, res) => {
   // console.log(req.body.image) //Cannot retrieve image information like this
   const img_filename = req.file.filename;
   const sql =
-    "INSERT INTO `boarding_house` (`Title`, `Description`, `Price`, `Address`, `Boys`, `Girls`, `Facilities`, `Rules`, `ContactNo`, `CoverImage`) VALUES(?)";
+    "INSERT INTO `boarding_house` (`Title`, `Description`, `Price`,`Negotiable`, `Address`, `Distance`, `Boys`, `Girls`, `Facilities`, `Rules`, `ContactNo`, `CoverImage`) VALUES(?)";
   console.log(img_filename + "___" + req.body.address);
   const values = [
     req.body.title,
     req.body.description,
     req.body.price,
+    req.body.negotiable,
     req.body.address,
+    req.body.distance,
     req.body.boys,
     req.body.girls,
     req.body.facilities,
@@ -151,9 +250,11 @@ const storage_multiple = multer.diskStorage({
     cb(null, "public/images");
   },
   filename: function (req, file, cb) {
+    const ext = path.extname(file.originalname); // Get the file extension
+    const filenameWithoutExt = path.basename(file.originalname, ext); // Get the filename without extension
     cb(
       null,
-      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+      file.fieldname + "-" + Date.now() + "-" + filenameWithoutExt + path.extname(file.originalname)
     );
   },
 });
@@ -196,7 +297,7 @@ app.post("/images/upload/:id", (req, res) => {
       const valuesString = JSON.stringify(values); // Convert array to JSON string
       const sql = "UPDATE `boarding_house` SET `OtherImages` = ? WHERE `Id` = ?";
       console.log(RowId);
-      console.log(valuesString);
+      console.log("valueString"+valuesString);
       
       db.query(sql, [valuesString, RowId], (err, data) => {
         if (err) return res.json({ Error: "Insert data Error in server" });
@@ -209,10 +310,47 @@ app.post("/images/upload/:id", (req, res) => {
 
 ///Owner - end
 
-app.get("/logout", (req, res) => {
-  req.session.destroy();
-  return res.json("success");
+
+//Fetch data for useEffect - start
+app.get('/', (req,res)=>{
+	if(req.session.role){
+		return res.json({Valid: true, Role: req.session.role })
+	} else {
+		return res.json({Valid: false})
+	}
+})
+app.get("/boarding-data", (req, res) => {
+  const sql = "SELECT * FROM boarding_house";
+  db.query(sql, (err, result) => {
+    if (err) {
+      console.error("Error executing MySQL query:", err);
+      res.status(500).json({ error: "Internal server error" });
+    } else {
+      res.json(result);
+    }
+  });
 });
+app.get("/boarding-data/:id", (req, res) => {
+  const RowId = req.params.id;
+  const sql = "SELECT * FROM boarding_house WHERE `Id` = ?";
+  db.query(sql, [RowId], (err, result) => {
+    if (err) {
+      console.error("Error executing MySQL query:", err);
+      res.status(500).json({ error: "Internal server error" });
+    } else {
+      //console.log(result);
+      res.json(result);
+    }
+  });
+});
+//Fetch data for useEffect - end
+
+app.get('/logout',(req,res)=>{
+  req.session.destroy(function (err) {
+    res.clearCookie('connect.sid');
+    res.redirect('/'); //Inside a callback… bulletproof!
+   });
+})
 
 app.listen(8081, () => {
   console.log("listening");
--- Web App/frontend/src/Pages/Home.jsx
+++ Web App/frontend/src/Pages/Home.jsx
@@ -1,10 +1,40 @@
-import React from "react";
-import '../css/home.css';
-import Header from '../Components/Header';
+import React, { useEffect, useState } from "react";
+import "../css/home.css";
+import Header from "../Components/Header";
 import Navbar from "../Components/Navbar";
 import SearchItem from "../Components/SearchItem";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+  const [data, setData] = useState([]);
+  const navigate = useNavigate();
+  axios.defaults.withCredentials = true;
+  useEffect(() => {
+    axios.get('http://localhost:8081')
+      .then((res) => {
+        if (res.data.Valid && (res.data.Role === 'student' || res.data.Role === 'owner')) {
+          console.log(res.data.Role)
+        } else{
+          console.log("Check this");
+          navigate('/')
+        }
+      })
+      .catch((err) => console.log(err))
+      // eslint-disable-next-line
+  }, [])
+  
+  useEffect(() => {
+    axios.get("http://localhost:8081/boarding-data")
+      .then((res) => {
+        console.log(res.data);
+        setData(res.data);
+      })
+      .catch((error) => {
+        console.error("Error, fetching data from backend:", error);
+      });
+      // eslint-disable-next-line
+  }, []);
   return (
     <div>
       <Navbar />
@@ -12,13 +42,9 @@ function Home() {
       <div className="listContainer">
         <div className="listWrapper">
           <div className="listResult">
-            <SearchItem />
-            <SearchItem />
-            <SearchItem />
-            <SearchItem />
-            <SearchItem />
-            <SearchItem />
-            <SearchItem />
+            {data.map((item) => (
+                <SearchItem key={item.id} data={item} />
+            ))}
           </div>
         </div>
       </div>
