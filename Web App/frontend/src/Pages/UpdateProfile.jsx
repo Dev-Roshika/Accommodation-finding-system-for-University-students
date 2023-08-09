986b7dfb6e53427b2b643bf533118955b7a338f6
IsharaMadushanka <60610077+IsharaMadushanka@users.noreply.github.com>
2023-08-09 21:15:58 +0530

    Web ishara madushanka (#33)
    * updates to Profile page.
    
    * more improvements to Profile page.
    
    * Validation for Update Profile.
    
    * sort feature is added.
    
    * improving sorting and adding delete profile functions
    
    * adding delete account and update password functions are added and some changes in CSS
    
    * improving features.
    
    * fixing bugs.

--- Web App/frontend/src/Pages/UpdateProfile.jsx
+++ Web App/frontend/src/Pages/UpdateProfile.jsx
@@ -10,8 +10,11 @@ function UpdateProfile() {
     const [role, setRole] = useState('');
     //const [user, setUser] = useState([]);
     const navigate = useNavigate();
-
+    const [emailError, setEmailError] = useState('');
+    const [contactNoError, setContactNoError] = useState('');
+   
     axios.defaults.withCredentials = true;
+
     useEffect(() => {
       axios.get('http://localhost:8081')
         .then((res) => {
@@ -40,14 +43,42 @@ function UpdateProfile() {
         getUser();
     },[])
     const Navigate = useNavigate();
+    const validateEmail = (email) => {
+    // Simple email validation using regex
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(email);
+  };
+
+  const validateContactNo = (contactNo) => {
+    // Remove all non-digit characters and check if the resulting string has a length of 10
+    const strippedContactNo = contactNo.replace(/\D/g, '');
+    return strippedContactNo.length === 10;
+  };
     const handleClick = async e =>{
         e.preventDefault();
+        const updatedUser = user[0];
+        const { Email, ContactNo } = updatedUser;
+       
+        // Validate email format
+        if (!validateEmail(Email)) {
+          setEmailError('Invalid email format');
+          return;
+        }
+        else setEmailError('')
+    
+        // Validate contact number length
+        if (!validateContactNo(ContactNo)) {
+          setContactNoError('Contact number should contain exactly 10 numbers');
+          return;
+        }
+        else setContactNoError('')
+
     try{
       await axios.put("http://localhost:8081/student/updateUser",user) 
-      console.log("work")
+      //console.log("work")
       Navigate("/profile")
     }catch(err){
-        console.log(user)
+       // console.log(user)
       console.log("error")
       console.log(err)
     }
@@ -68,30 +99,46 @@ function UpdateProfile() {
 <div>
      <Navbar />
      
-  <div className="container">
-  <div className= "square">     
+      
       <div className="profile">
             {user.map((cuser,index) =>(
-                <div key={cuser.Id}>
+                <div className='containerProfile' key={cuser.Id}>
+                  <div className='left'>
                     <div  className='profileimage'>
                         <img  className="profileimg" src={`http://localhost:8081/images/profile_images/${role}/${cuser.ProfileImage}`} alt='' height={100} width={100} />
                     </div> 
-                    <div className="rows">                    <p className='row_'><label>User Name :  {cuser.UserName}</label></p>
-                      <p className='row_'><label>Full Name :  {cuser.FullName}</label></p>
-                       
-                     <p className='row_'> <input className='profileInput' type="text" placeholder={cuser.Email}  name="Email" value={cuser.Email||''} onChange={(e) => handleInputChange(e, index)}/>
-                      </p>
-                      <p className='row_'><input className='profileInput' type="number" placeholder={cuser.ContactNo} value={cuser.ContactNo||''} onChange={(e) => handleInputChange(e, index)}  name = "ContactNo"/>
-              </p>   
-              <button className='profileupdate' onClick={handleClick}>Save</button>
+                  </div>
+                  <div className='right'>
+                  <div className="rows"> 
+                       <p className='row'><label>User Name :  {cuser.UserName}</label></p>
+                      <p className='row'><label>Full Name :  {cuser.FullName}</label></p>
+                       {role ==='student'?(<>
+                        <p className='row'><label>Unoversity RegNo :  {cuser.UnivRegNo}</label></p>
+                        <p className='row'><label>Faculty :  {cuser.Faculty}</label></p>
+                        <p className='row'><label>Department :  {cuser.Dept}</label></p>
+                       </>)
+                       :role === 'owner'?(<>
+                        <p className='row'><label>NID :  {cuser.NidNo}</label></p>
+                       <p className='row'> <label>Address :<input className='profileInput' type="text" placeholder={cuser.PrivateAddress}  name="PrivateAddress" value={cuser.PrivateAddress||''} onChange={(e) => handleInputChange(e, index)}/>
+                       </label></p>
+                       </>):null}
+
+                     <p className='row'> <label>Email : <input className='profileInput' type="text" placeholder={cuser.Email}  name="Email" value={cuser.Email||''} onChange={(e) => handleInputChange(e, index)}/>
+                      {emailError && <span className="error">{emailError}</span>}</label></p>
+                      
+                      <p className='row'><label> Contact No : <input className='profileInput' type='tel' maxLength={10} placeholder={cuser.ContactNo} value={cuser.ContactNo||''} onChange={(e) => handleInputChange(e, index)}  name = "ContactNo"/>
+                      {contactNoError && <span className="error">{contactNoError}</span>}</label></p>  
+
+                      <button className='profileupdate' onClick={handleClick}>Save</button>
                     </div>
+                  </div>
+                    
                 </div>
             ))}
 
   </div>
   </div>   
-        </div>  
-</div>
+        
   )
 }
 
